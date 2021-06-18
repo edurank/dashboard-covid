34adc6c16a7065c0db41c8e3f4250841e3b895aa
edurank <vinci.luiz@gmail.com>
2021-06-18 18:16:10 -0300

    Timing e aviso da mensagem sem mascara

--- config/test.js
+++ config/test.js
@@ -1,5 +1,18 @@
-const db = require('db');
+let tempo = null;
+let esperando = false;
 
-function addUser() {
-  db.getFuncionarios();
+function espera() {
+  esperando = true;
+  setTimeout(function() { esperando = false }, 2000);
+}
+
+
+
+while(true) {
+  
+  if(false) {
+    espera();
+    tempo = setTimeout(function() { console.log("teste") }, 3000);
+    clearTimeout(tempo);
+  }
 }
